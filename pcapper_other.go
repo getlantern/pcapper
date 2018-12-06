4f0e260a171e4164ecf158171d60d7eee06e6afb
Ox Cart <ox.to.a.cart@gmail.com>
2018-12-06 14:03:44 -0800

    Fixed non-linux version of Dump function

--- pcapper_other.go
+++ pcapper_other.go
@@ -7,6 +7,6 @@ func StartCapturing(interfaceName string, dir string, numIPs int, packetsPerIP i
 }
 
 // Dump doesn't do anything on this platform.
-func Dump(ip string) error {
+func Dump(prefix string, ip string) error {
 	return nil
 }
